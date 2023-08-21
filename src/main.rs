b8893d2df55737a63a08ab3bf94c4aa7b7f77b1f
musa <kerimlimusa@gmail.com>
2023-08-21 22:41:04 +0400

    dynamic port binding in tests

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
+    let port = listener.local_addr().unwrap().port();
+    println!("{}", port);
+    run(listener)?.await
 }
