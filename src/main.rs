e7498bd528b0ad2df0ebc6502bcb4381fc4d24a9
m <musa.karimli@pashapay.az>
2023-09-04 00:06:30 +0400

    4.5.12

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
